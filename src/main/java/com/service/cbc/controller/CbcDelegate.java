33fa7531c4a37d4f1e8370edb9c115f7c660dff2
paas <paas@cse-codegen-backend-963378786-b13t3>
2017-12-02 10:48:44 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cbc/controller/CbcDelegate.java
@@ -0,0 +1,14 @@
+package com.service.cbc.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CbcDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
---
+++ src/main/java/com/service/cbc/controller/CbcImpl.java
@@ -0,0 +1,29 @@
+package com.service.cbc.controller;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import io.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseJAXRSServerDemoCodegen", date = "2017-12-02T10:48:44.646Z")
+
+
+@RestSchema(schemaId = "cbc")
+@Path("/cbc")
+
+@Produces({ "application/json" })
+public class CbcImpl  {
+
+    @Autowired
+    private CbcDelegate cbcDelegate;
+
+    @Path("/helloworld")
+    @GET
+    
+    @Produces({ "application/json" })
+    public String helloworld(@QueryParam("name") String name) {
+
+    	return cbcDelegate.helloworld(name);
+    }
+}
+
